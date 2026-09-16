7220076dadd5c664127294e84d1066b577325534
P47Parzival <dhruvmali999@gmail.com>
2026-09-16 22:25:11 +0530

    setting maxDuration: 60, for free vercel plan

--- api/upload.js
+++ api/upload.js
@@ -1,7 +1,5 @@
 import { processUpload } from '../lib/processUpload.js'
 
-export const config = { maxDuration: 60 }
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method not allowed' })
